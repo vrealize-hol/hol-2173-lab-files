dcc8c1f9ecb0df0dfd574e327390b9af0750cdd5
Benoit <ben@bserratrice-a01.vmware.com>
2021-01-27 12:22:23 +0800

    Add TKC logs info

--- automation/pre-config.bat
+++ automation/pre-config.bat
@@ -2,4 +2,8 @@
 cd %~dp0
 
 python kubectl-vsphere-login.py
+
+kubectl config get-contexts
+kubectl get tkc/tf-runtime -n hol-tf-service -o=jsonpath={.spec.topology.workers.count}
+
 kubectl apply -f tf-runtime.yaml
